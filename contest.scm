bf7fd738bc407a9ddbd06a88b35e12bb29e477ad
Ming Fong <mingfong07@hotmail.com>
2021-04-29 19:11:25 -0700

    cooler

--- contest.scm
+++ contest.scm
@@ -14,21 +14,26 @@
   ; RNG from here:
   ; https://stackoverflow.com/questions/14674165/scheme-generate-random
   (define random
-  (let ((a 69069) (c 1) (m (expt 2 32)) (seed 19380110))
+  (let ((a 69069) (c 1) (m (expt 2 32)) (seed 12345))
     (lambda (new-seed)
       (if (pair? new-seed)
           (set! seed (car new-seed))
           (set! seed (modulo (+ (* seed a) c) m)))
       (/ seed m))))
-  
+  (define (random-choice a b)
+    (if (zero? (random 2)) a b)
+  )
+  (bgcolor "black")
+  (speed 0)
   (pendown)
   (define (loop n)
     (if (= n 0)
       ()
       (begin
         (color (rgb (random n) (random n) (random n)))
-        (right (* 360 (random 4)))
-        (forward (* 1000 (random 5)))
+        ; (right (* 360 (random 9)))
+        (right (* 60 (random-choice 0 1)))
+        (forward (* 200 (random 69)))
         (loop (- n 1))
       )
       
@@ -36,10 +41,15 @@
   )
   ; 119 max recursion depth
   (loop 119)
-
-
+  (loop 50)
+;   (loop 119)
+;   (loop 119)
+;   (loop 119)
+;   (loop 119)
+;   (loop 119)
 
   (hideturtle)
+;   (save-to-file "asdf")
   (exitonclick)
 )
 
